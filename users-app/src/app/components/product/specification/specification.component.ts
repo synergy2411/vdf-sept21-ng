2d35960f428c152b49fae66d812564658a5f5b0a
Sumit K <synergy2411@outlook.com>
2021-09-23 16:30:34 +0530

    query params added

--- users-app/src/app/components/product/specification/specification.component.ts
+++ users-app/src/app/components/product/specification/specification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-specification',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecificationComponent implements OnInit {
 
-  constructor() { }
+  description : string;
+
+  constructor(private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.description = params['description']
+    })
   }
 
 }
